7d42b2558ffc85f49a2f4246a6a5ff954a4916aa
JoanBaucells <joanbaucellsnualart@gmail.com>
2020-02-21 16:18:50 +0100

    tete

--- main.js
+++ main.js
@@ -2,3 +2,34 @@
 // SLIDER code
 
 // other code
+window.onload = function(){
+
+  var videos =[
+    {nom:'Bunny',arxiu:'bunny.mp4',imatge:'resources/image/haaland.jpg'},
+    {nom:'Trailer',arxiu:'trailer.mp4',imatge:'resources/image/stadium.jpg'}
+];
+var requestURL = "resources/json/clubs.json";
+
+  var request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function(){
+      var clubs = request.response;
+  }
+
+videos.forEach(element => {
+  var li = this.document.createElement("li");
+  var img = this.document.createElement("img");
+  var h1 = this.document.createElement("h1");
+  h1.classList.add("flex-caption");
+  h1.textContent = element.nom;
+  img.src = element.imatge;
+  li.appendChild(img);
+  li.appendChild(h1);
+  this.document.querySelector(".slides").appendChild(li);
+});
+
+}
